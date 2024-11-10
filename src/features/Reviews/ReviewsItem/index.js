a3d3752f0e651da4fa5d0be8dc787b888997cd4b
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-11-10 09:47:39 +0100

    added change for SEO

--- src/features/Reviews/ReviewsItem/index.js
+++ src/features/Reviews/ReviewsItem/index.js
@@ -8,9 +8,22 @@ import {
   Data,
 } from "./styled";
 import { Text } from "../../../common/Text";
+import { getContentForReview } from "./getContentForReview";
 
 export const ReviewsItem = ({ item }) => (
   <ItemWrapper itemScope itemType="https://schema.org/Review">
+    <div itemProp="reviewRating" itemScope itemType="https://schema.org/Rating">
+      <meta itemProp="ratingValue" content={item.rating} />
+      <meta itemProp="bestRating" content="5" />
+    </div>
+    <div itemProp="itemReviewed" itemScope itemType="https://schema.org/LocalBusiness">
+      <meta itemProp="name" content="Serwis RTV i AGD" />
+      <meta itemProp="description" content="Naprawa pralek, zmywarek, ekspresów do kawy oraz telewizorów" />
+    </div>
+    <div itemProp="service" itemScope itemType="https://schema.org/Service">
+      <meta itemProp="name" content={getContentForReview(item.text)} />
+      <meta itemProp="serviceType" content={`Profesjonalna ${getContentForReview(item.text)} - Szybko i solidnie`} />
+    </div>
     <Header itemProp="author" itemScope itemType="https://schema.org/Person">
       <Photo
         itemProp="image"
