6b2d059ebe7493df8824ed4030e9928784dffda7
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-11-10 08:43:47 +0100

    added change for SEO

--- src/features/Reviews/ReviewsItem/Stars/index.js
+++ src/features/Reviews/ReviewsItem/Stars/index.js
@@ -8,12 +8,14 @@ export const Stars = ({ rating }) => {
         <meta itemProp="ratingValue" content={rating} />
         <meta itemProp="bestRating" content="5" />
       </div>
-      <div itemProp="itemReviewed" itemScope itemType="https://schema.org/Service">
-        <meta itemProp="name" content="Naprawa pralek" />
-        <meta itemProp="description" content="Usługi naprawy pralek, zmywarek, ekspresów do kawy oraz telewizorów" />
-        <meta itemProp="serviceType" content="Naprawa sprzętu AGD" />
-        <meta itemProp="category" content="Naprawa sprzętu AGD" />
-        <meta itemProp="type" content="LocalBusiness" />
+      <div itemProp="itemReviewed" itemScope itemType="https://schema.org/LocalBusiness">
+        <meta itemProp="name" content="Serwis RTV i AGD" />
+        <meta itemProp="description" content="Naprawa pralek, zmywarek, ekspresów do kawy oraz telewizorów" />
+
+        <div itemProp="service" itemScope itemType="https://schema.org/Service">
+          <meta itemProp="name" content="Naprawa pralek" />
+          <meta itemProp="serviceType" content="Naprawa sprzętu AGD" />
+        </div>
       </div>
       <StarsWrapper>
         <Star $active={rating >= 1} />
