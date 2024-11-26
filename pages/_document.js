9ea9eeb7ab8dcc26fa576123b1dbcf1fb86b67e5
Mariusz Matusiewicz <mariuszmmm@op.pl>
2024-11-26 07:35:11 +0100

    meta tags corection

--- pages/_document.js
+++ pages/_document.js
@@ -36,19 +36,19 @@ export default class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="theme-color" content="#000000" />
           <meta name="author" content="Naprawa pralek i sprzętu RTV" />
-          <meta property="og:image" content="/share.png" />
+          <meta property="og:image" content="https://naprawaprzemysl.pl/share.png" />
           <meta property="fb:app_id" content="100063811592941" />
           <meta property="og:type" content="website" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link rel="preload" href="https://fonts.googleapis.com/css?family=Lato:200,400,400italic,600&display=swap" as="style" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:200,400,400italic,600&display=swap" media="all" />
-          <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" href="https://naprawaprzemysl.pl/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="https://naprawaprzemysl.pl/favicon.svg" />
+          <link rel="shortcut icon" href="https://naprawaprzemysl.pl/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="https://naprawaprzemysl.pl/apple-touch-icon.png" />
           <meta name="apple-mobile-web-app-title" content="MyWebSite" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="https://naprawaprzemysl.pl/site.webmanifest" />
         </Head>
         <body>
           <Main />
