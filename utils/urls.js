c5ecf8e6d17b81f473b956a8fd36ca98aef553f1
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-09 06:33:06 +0200

    test

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
 
 export const cloudinaryImageUrls = {
   serwis: "https://res.cloudinary.com/difc0i71u/image/upload/v1731168333/Serwis/serwis.webp",
@@ -24,35 +24,35 @@ export const cloudinaryImageUrls = {
 };
 
 export const imageUrls = {
-  serwis: baseUrl + "images/serwis-rtv-agd-naprawa-przemysl.webp",
-  serwis_1: baseUrl + "images/serwis.jpg",
-  telewizor: baseUrl + "images/serwis-telewizorow-przemysl-naprawa-telewizora.webp",
-  telewizor_300: baseUrl + "images/serwis-telewizorow-przemysl-naprawa-telewizora_300.webp",
-  ekspres: baseUrl + "images/serwis-ekspresow-przemysl-naprawa-ekspresu.webp",
-  ekspres_300: baseUrl + "images/serwis-ekspresow-przemysl-naprawa-ekspresu_300.webp",
-  ekspres_2: baseUrl + "images/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu.webp",
-  ekspres_2_300: baseUrl + "images/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu_300.webp",
-  zmywarka: baseUrl + "images/naprawa-zmywarek-przemysl-serwis-zmywarki.webp",
-  zmywarka_300: baseUrl + "images/serwis-zmywarek-przemysl-naprawa-zmywarki_300.webp",
-  pralka: baseUrl + "images/naprawa-pralek-przemysl-serwis-pralki.webp",
-  pralka_300: baseUrl + "images/serwis-pralek-przemysl-naprawa-pralki_300.webp",
-  suszarka: baseUrl + "images/naprawa-suszarek-przemysl-serwis-suszarki.webp",
-  suszarka_300: baseUrl + "images/serwis-suszarek-przemysl-naprawa-suszarki_300.webp",
-  logoGoogle: baseUrl + "images/logoGoogle.webp",
-  logo: baseUrl + "images/logo.svg",
-  mapa: baseUrl + "images/mapa.webp",
+  serwis: baseUrl + "/images/serwis-rtv-agd-naprawa-przemysl.webp",
+  serwis_1: baseUrl + "/images/serwis.jpg",
+  telewizor: baseUrl + "/images/serwis-telewizorow-przemysl-naprawa-telewizora.webp",
+  telewizor_300: baseUrl + "/images/serwis-telewizorow-przemysl-naprawa-telewizora_300.webp",
+  ekspres: baseUrl + "/images/serwis-ekspresow-przemysl-naprawa-ekspresu.webp",
+  ekspres_300: baseUrl + "/images/serwis-ekspresow-przemysl-naprawa-ekspresu_300.webp",
+  ekspres_2: baseUrl + "/images/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu.webp",
+  ekspres_2_300: baseUrl + "/images/serwis-ekspresow-przemysl-profesjonalna-naprawa-ekspresu_300.webp",
+  zmywarka: baseUrl + "/images/naprawa-zmywarek-przemysl-serwis-zmywarki.webp",
+  zmywarka_300: baseUrl + "/images/serwis-zmywarek-przemysl-naprawa-zmywarki_300.webp",
+  pralka: baseUrl + "/images/naprawa-pralek-przemysl-serwis-pralki.webp",
+  pralka_300: baseUrl + "/images/serwis-pralek-przemysl-naprawa-pralki_300.webp",
+  suszarka: baseUrl + "/images/naprawa-suszarek-przemysl-serwis-suszarki.webp",
+  suszarka_300: baseUrl + "/images/serwis-suszarek-przemysl-naprawa-suszarki_300.webp",
+  logoGoogle: baseUrl + "/images/logoGoogle.webp",
+  logo: baseUrl + "/images/logo.svg",
+  mapa: baseUrl + "/images/mapa.webp",
 };
 
 export const appUrls = {
-  home: baseUrl,
-  kontakt: baseUrl + "kontakt/",
-  o_mnie: baseUrl + "o-mnie/",
-  opinie: baseUrl + "opinie/",
-  naprawa_pralek: baseUrl + "naprawa-pralek/",
-  naprawa_suszarek: baseUrl + "naprawa-suszarek/",
-  naprawa_zmywarek: baseUrl + "naprawa-zmywarek/",
-  naprawa_ekspresow: baseUrl + "naprawa-ekspresow/",
-  naprawa_telewizorow: baseUrl + "naprawa-telewizorow/",
+  home: baseUrl + "/",
+  kontakt: baseUrl + "/kontakt/",
+  o_mnie: baseUrl + "/o-mnie/",
+  opinie: baseUrl + "/opinie/",
+  naprawa_pralek: baseUrl + "/naprawa-pralek/",
+  naprawa_suszarek: baseUrl + "/naprawa-suszarek/",
+  naprawa_zmywarek: baseUrl + "/naprawa-zmywarek/",
+  naprawa_ekspresow: baseUrl + "/naprawa-ekspresow/",
+  naprawa_telewizorow: baseUrl + "/naprawa-telewizorow/",
 };
 
-export const dataUrl = baseUrl === "/" ? "http://localhost:3000/data.json" : `${baseUrl}/data.json`;
+export const dataUrl = baseUrl ? `${baseUrl}/data.json` : "http://localhost:3000/data.json";
