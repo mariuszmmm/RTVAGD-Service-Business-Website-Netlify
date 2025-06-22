b25736ee360b4aa77239d320190db037742d83c4
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-22 08:20:12 +0200

    test

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://naprawaprzemysl.pl',
-  generateRobotsTxt: true,
-  sitemapSize: 5000,
-  outDir: './public',
-}
