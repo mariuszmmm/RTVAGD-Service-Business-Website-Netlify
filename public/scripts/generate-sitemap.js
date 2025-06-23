20de3eb69cad3bc8ff8b234612f79f9302d5505e
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-23 08:31:12 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -68,15 +68,15 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     .map(([route]) => {
       return `
   <url>
-    <loc>${BASE_URL}${route} </loc>
-    <lastmod>${new Date().toISOString()} </lastmod>
+    <loc>${BASE_URL}${route}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>daily</changefreq>
-    <priority>${routes[route].priority || '0.8'} </priority>
+    <priority>${routes[route].priority || '0.8'}</priority>
     <mobile:mobile/>
     <image:image>
-      <image:loc>${routes[route].image} </image:loc>
-      <image:caption>${routes[route].caption} </image:caption>
-      <image:title>${routes[route].title} </image:title>
+      <image:loc>${routes[route].image}</image:loc>
+      <image:caption>${routes[route].caption}</image:caption>
+      <image:title>${routes[route].title}</image:title>
     </image:image>
   </url>`;
     })
