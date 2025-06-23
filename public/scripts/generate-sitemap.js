f523386f6c10d55dbb2c0e359256536b761df7ba
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-23 09:38:51 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -62,18 +62,18 @@ const routes = {
 const urlsXml = Object.entries(routes)
   .map(([route]) => {
     return [
-      `<url>`,
-      `  <loc>${BASE_URL}${route}</loc>`,
-      `   <lastmod>${new Date().toISOString()}</lastmod>`,
-      `   <changefreq>daily</changefreq>` +
-      `   <priority>${routes[route].priority || '0.8'}</priority>`,
-      `   <mobile:mobile/>`,
-      `   <image:image>`,
-      `     <image:loc>${routes[route].image}</image:loc>`,
-      `     <image:caption>${routes[route].caption}</image:caption>`,
-      `     <image:title>${routes[route].title}</image:title>`,
-      `   </image:image>`,
-      `</url>`
+      ` <url>`,
+      `   <loc>${BASE_URL}${route}</loc>`,
+      `    <lastmod>${new Date().toISOString()}</lastmod>`,
+      `    <changefreq>daily</changefreq>` +
+      `    <priority>${routes[route].priority || '0.8'}</priority>`,
+      `    <mobile:mobile/>`,
+      `    <image:image>`,
+      `      <image:loc>${routes[route].image}</image:loc>`,
+      `      <image:caption>${routes[route].caption}</image:caption>`,
+      `      <image:title>${routes[route].title}</image:title>`,
+      `    </image:image>`,
+      ` </url>`
     ].join('\n');;
   })
   .join('\n');
