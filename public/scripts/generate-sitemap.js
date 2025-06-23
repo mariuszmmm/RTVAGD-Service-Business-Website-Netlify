6527e499dc4f7e3b5ce423d87e56f48637389bb8
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-23 09:23:53 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -62,17 +62,19 @@ const routes = {
 const urlsXml = `
   ${Object.entries(routes)
     .map(([route]) => {
-      return `  
-    <loc>${BASE_URL}${route}</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>${routes[route].priority || '0.8'}</priority>
-    <mobile:mobile/>
-    <image:image>
-      <image:loc>${routes[route].image}</image:loc>
-      <image:caption>${routes[route].caption}</image:caption>
-      <image:title>${routes[route].title}</image:title>
-    </image:image>
+      return `
+    <url>  
+      <loc>${BASE_URL}${route}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>${routes[route].priority || '0.8'}</priority>
+      <mobile:mobile/>
+      <image:image>
+        <image:loc>${routes[route].image}</image:loc>
+        <image:caption>${routes[route].caption}</image:caption>
+        <image:title>${routes[route].title}</image:title>
+      </image:image>
+    </url>
   `;
     })
     .join('\n')}
@@ -82,9 +84,7 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n` +
   `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n` +
   `  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"\n` +
   `  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n` +
-  `   <url>` +
   urlsXml +
-  `   </url>` +
   `</urlset>`;
 
 const outputPath = path.join(__dirname, '..', 'test.xml');
