282ca3e552d4850ecc6a1858dfcfdb9eb871b4cc
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-23 09:26:26 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -63,21 +63,20 @@ const urlsXml = `
   ${Object.entries(routes)
     .map(([route]) => {
       return `
-    <url>  
-      <loc>${BASE_URL}${route}</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>daily</changefreq>
-      <priority>${routes[route].priority || '0.8'}</priority>
-      <mobile:mobile/>
-      <image:image>
-        <image:loc>${routes[route].image}</image:loc>
-        <image:caption>${routes[route].caption}</image:caption>
-        <image:title>${routes[route].title}</image:title>
-      </image:image>
-    </url>
+  <url>  
+    <loc>${BASE_URL}${route}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>${routes[route].priority || '0.8'}</priority>
+    <mobile:mobile/>
+    <image:image>
+      <image:loc>${routes[route].image}</image:loc>
+      <image:caption>${routes[route].caption}</image:caption>
+      <image:title>${routes[route].title}</image:title>
+    </image:image>
+  </url>
   `;
-    })
-    .join('\n')}
+    })}
 `;
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n` +
