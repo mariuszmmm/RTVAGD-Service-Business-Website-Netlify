e3e7531e0f545880f7b08b668657df50f01363a9
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-23 09:36:14 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -59,31 +59,30 @@ const routes = {
   },
 }
 
-const urlsXml = `
-  ${Object.entries(routes)
-    .map(([route]) => {
-      return [
-        `<url>`,
-        `  <loc>${BASE_URL}${route}</loc>`,
-        `   <lastmod>${new Date().toISOString()}</lastmod>`,
-        `   <changefreq>daily</changefreq>` +
-        `   <priority>${routes[route].priority || '0.8'}</priority>`,
-        `   <mobile:mobile/>`,
-        `   <image:image>`,
-        `     <image:loc>${routes[route].image}</image:loc>`,
-        `     <image:caption>${routes[route].caption}</image:caption>`,
-        `     <image:title>${routes[route].title}</image:title>`,
-        `   </image:image>`,
-        `</url>`
-      ];
-    })}
-`;
+const urlsXml = Object.entries(routes)
+  .map(([route]) => {
+    return [
+      `<url>`,
+      `  <loc>${BASE_URL}${route}</loc>`,
+      `   <lastmod>${new Date().toISOString()}</lastmod>`,
+      `   <changefreq>daily</changefreq>` +
+      `   <priority>${routes[route].priority || '0.8'}</priority>`,
+      `   <mobile:mobile/>`,
+      `   <image:image>`,
+      `     <image:loc>${routes[route].image}</image:loc>`,
+      `     <image:caption>${routes[route].caption}</image:caption>`,
+      `     <image:title>${routes[route].title}</image:title>`,
+      `   </image:image>`,
+      `</url>`
+    ];
+  })
+  .join('\n');
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n` +
   `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n` +
   `  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"\n` +
   `  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n` +
-  urlsXml.join('\n') +
+  urlsXml + '\n' +
   `</urlset>`;
 
 const outputPath = path.join(__dirname, '..', 'test.xml');
