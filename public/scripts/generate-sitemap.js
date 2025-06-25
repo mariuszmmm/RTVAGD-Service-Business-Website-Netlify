410023c9363005bfbe9779daad4716c11792afe6
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-25 05:31:00 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -63,23 +63,24 @@ const routes = {
 const urlsXml = Object.entries(routes)
   .map(([route]) => {
     const data = routes[route];
+    const imageBlock = data.image && data.caption && data.title
+      ? [
+        `    <image:image>`,
+        `      <image:loc>${data.image}</image:loc>`,
+        `      <image:caption>${data.caption}</image:caption>`,
+        `      <image:title>${data.title}</image:title>`,
+        `    </image:image>`
+      ].join('\n')
+      : '';
     return [
       `  <url>`,
       `    <loc>${BASE_URL}${route}</loc>`,
       `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>`,
       `    <changefreq>${data.changefreq || 'daily'}</changefreq>`,
       `    <priority>${data.priority || '0.8'}</priority>`,
-      data.image && data.caption && data.title
-        ? [
-          `    <image:image>`,
-          `      <image:loc>${data.image}</image:loc>`,
-          `      <image:caption>${data.caption}</image:caption>`,
-          `      <image:title>${data.title}</image:title>`,
-          `    </image:image>`
-        ].join('\n')
-        : '',
+      imageBlock,
       `  </url>`
-    ].join('\n');;
+    ].filter(Boolean).join('\n');
   })
   .join('\n');
 
