e944c24b37c74341392ce823eec2416fe36ec042
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-25 05:12:55 +0200

    test

--- public/scripts/generate-sitemap.js
+++ public/scripts/generate-sitemap.js
@@ -64,10 +64,9 @@ const urlsXml = Object.entries(routes)
     return [
       `  <url>`,
       `    <loc>${BASE_URL}${route}</loc>`,
-      `    <lastmod>${new Date().toISOString()}</lastmod>`,
+      `    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>`,
       `    <changefreq>daily</changefreq>`,
       `    <priority>${routes[route].priority || '0.8'}</priority>`,
-      `    <mobile:mobile/>`,
       `    <image:image>`,
       `      <image:loc>${routes[route].image}</image:loc>`,
       `      <image:caption>${routes[route].caption}</image:caption>`,
@@ -80,7 +79,6 @@ const urlsXml = Object.entries(routes)
 
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n` +
   `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n` +
-  `  xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"\n` +
   `  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n` +
   urlsXml + '\n' +
   `</urlset>`;
