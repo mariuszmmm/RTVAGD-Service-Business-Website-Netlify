eb7991c3b4e0d1ff3d2e5eea8f163883125556d9
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-06-29 10:29:05 +0200

    test

--- components/QrCode/styled.js
+++ components/QrCode/styled.js
@@ -72,6 +72,7 @@ export const ImageWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: end;
+  padding-top: 20px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
     width: 25px;
