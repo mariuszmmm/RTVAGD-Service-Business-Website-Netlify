467173e9207cee4346e3bf7622f665ec05c5247c
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-08-07 05:24:28 +0200

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   // output: 'export',
   images: {
     // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   compiler: {
     styledComponents: true,
