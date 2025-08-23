c5a3ff952635cf502e52391bc27a86f4b4add059
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-08-23 11:56:33 +0200

    test

--- utils/getImageData.js
+++ utils/getImageData.js
@@ -1,37 +1,35 @@
 import axios from 'axios';
 import { getCldImageUrl } from "next-cloudinary";
 import { imageDataUrl } from './urls';
-import { imageParameters } from './imagesParametrs';
 
 export const getImageData = async () => {
   try {
     const response = await axios(imageDataUrl);
     const { naprawa_zmywarek } = response.data || {};
 
-    // const getImageUrl = ({ src, width, version }) => getCldImageUrl({
-    //   src,
-    //   width,
-    //   crop: 'limit',
-    //   quality: 'auto',
-    //   fetchFormat: 'auto',
-    //   version,
-    // }).split('?')[0];
+    const getImageUrl = ({ src, width, version }) => getCldImageUrl({
+      src,
+      width,
+      crop: 'limit',
+      quality: 'auto',
+      fetchFormat: 'auto',
+      version,
+    }).split('?')[0];
 
-    // const widths = [190, 284, 380, 425, 480, 520, 568, 760, 850, 1024];
-    // const getSrcSet = ({ src, version }) => widths.map(width => `${getImageUrl({ src, width, version })} ${width}w`).join(', ');
+    const widths = [190, 284, 380, 425, 480, 520, 568, 760, 850, 1024];
+    const getSrcSet = ({ src, version }) => widths.map(width => `${getImageUrl({ src, width, version })} ${width}w`).join(', ');
 
-    // const imageParameters = {
-    //   zmywarka: {
-    //     url: getImageUrl({ src: naprawa_zmywarek.public_id, width: naprawa_zmywarek.width, version: naprawa_zmywarek.version }),
-    //     srcSet: getSrcSet({ src: naprawa_zmywarek.public_id, version: naprawa_zmywarek.version }),
-    //     width: naprawa_zmywarek.width,
-    //     height: naprawa_zmywarek.height,
-    //   },
-    // };
+    const imageParameters = {
+      zmywarka: {
+        url: getImageUrl({ src: naprawa_zmywarek.public_id, width: naprawa_zmywarek.width, version: naprawa_zmywarek.version }),
+        srcSet: getSrcSet({ src: naprawa_zmywarek.public_id, version: naprawa_zmywarek.version }),
+        width: naprawa_zmywarek.width,
+        height: naprawa_zmywarek.height,
+      },
+    };
 
     return {
-      // imageParameters: imageParameters || null
-      imageParameters
+      imageParameters: imageParameters || null
     };
   } catch (error) {
     console.error('Error fetching ImageData', error);
