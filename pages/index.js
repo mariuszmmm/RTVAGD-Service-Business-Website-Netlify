dc9b8e20920d7b777c977bcd075c46fdb7753e97
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-08-23 14:00:22 +0200

    test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import Home from './home';
 import { getData } from '../utils/getData';
+import { getDataForMetaTags } from '../utils/dataForMetaTags';
+import { getImageParameters } from '../utils/imagesParametrs';
+
+// export const getStaticProps = async () => {
+//   const data = await getData();
+
+//   console.log("TEST", data)
+
+//   return { props: { ...data || null } };
+// };
 
 export const getStaticProps = async () => {
+  const dataForMetaTags = await getDataForMetaTags();
+  const imageParameters = await getImageParameters();
   const data = await getData();
+  // console.log("dataForMetaTags", { dataForMetaTags })
 
-  console.log("TEST", data)
-
-  return { props: { ...data || null } };
+  return {
+    props: {
+      ...(data || null),
+      imageParameters: imageParameters || null,
+      dataForMetaTags: dataForMetaTags || null,
+    },
+  };
 };
 
 export default Home;
