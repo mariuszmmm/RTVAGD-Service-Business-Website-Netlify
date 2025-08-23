97541bb27b33c74c8b01c05da34a715aa97398e3
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-08-23 07:17:37 +0200

    test

--- utils/imagesParametrs.js
+++ utils/imagesParametrs.js
@@ -7,11 +7,10 @@ const getImageUrl = ({ src, width, version }) => getCldImageUrl({
   quality: 'auto',
   fetchFormat: 'auto',
   version,
-  dpr: 'auto',
 }).split('?')[0];
 
 const src = "Serwis/naprawa-zmywarek";
-const widths = [190, 284, 380, 425, 480, 520];
+const widths = [190, 284, 380, 425, 480, 520, 568, 760, 850, 1040, 1560];
 const getSrcSet = ({ version }) => widths.map(width => `${getImageUrl({ src, width, version })} ${width}w`).join(', ');
 
 export const imageParameters = {
