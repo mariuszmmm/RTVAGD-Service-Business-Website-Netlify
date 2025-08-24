71244c4aaff432fcc1d17463375bf8eceb74b932
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-08-24 18:12:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const nextConfig = {
   reactStrictMode: true,
-//  output: 'export',
+  output: 'export',
   images: { unoptimized: true },
   compiler: { styledComponents: true },
   webpack(config) {
