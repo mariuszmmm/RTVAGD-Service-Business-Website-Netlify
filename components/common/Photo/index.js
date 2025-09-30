e79b9067aa34e4d10b3ec906a24b16c89329dcac
Mariusz Matusiewicz <mariuszmmm@op.pl>
2025-09-30 14:49:35 +0200

    test

--- components/common/Photo/index.js
+++ components/common/Photo/index.js
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Photo = styled.img`
   width: 59vw;
   height: auto;
-  max-width: ${({ maxWidth }) => maxWidth || '100%'};
+  max-width: ${({ $maxWidth }) => $maxWidth || '100%'};
 `;
